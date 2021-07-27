34c740ef90cbe0a80150fe21c652e8bf80d717da
unknown <ssgsj.carranza@gmail.com>
2021-07-26 22:02:08 -0500

    began styling on search bar and imported icons, corrected imports as well

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Avatar from '../components/Avatar';
-import {MicroPhoneIcon, ViewGridIcon} from '@heroicons/react/solid';
+import {MicrophoneIcon, ViewGridIcon} from '@heroicons/react/solid';
+import { SearchIcon } from '@heroicons/react/outline';
 
 
 export default function Home() {
@@ -37,6 +38,11 @@ export default function Home() {
       {/* Body */}
         <form>
           < Image src='https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png' height={100} width={300}/>
+          <div className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center'>
+            <SearchIcon className='h-5 mr-3 text-gray-500'/>
+            <input type='text' className='focus:outline-none flex-grow'/>
+            <MicrophoneIcon className='h-5'/>
+          </div>
         </form>
       
       {/* Footer */}
