15ad5599fa176d73bb0c30d38c95cb00858fb91b
unknown <ssgsj.carranza@gmail.com>
2021-07-26 21:46:55 -0500

    changed config file to accept image being pulled for main page

--- pages/index.js
+++ pages/index.js
@@ -14,23 +14,31 @@ export default function Home() {
       </Head>
       {/* Header */}
       <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+        
         {/* Left */}
         <div className='flex space-x-4 items-center'>
           <p className='link'>About</p>
           <p className='link'>Store</p>
         </div>
+        
         {/* Right */}
         <div className='flex space-x-4 items-center'>
           <p className='link'>Gmail</p>
           <p className='link'>Images</p>
+          
           {/* Icon */}
           <ViewGridIcon className='h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer'/>
+          
           {/* Avatar */}
           <Avatar url='https://media.comicbook.com/2018/09/goku-dragon-ball-super-1135574-1280x0.jpeg'/>
         </div>
       </header>
+      
       {/* Body */}
-
+        <form>
+          < Image src='https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png' height={100} width={300}/>
+        </form>
+      
       {/* Footer */}
     </div>
   )
