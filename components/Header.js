153a22130546dda6716f010151f4eb6fe9b0076c
unknown <ssgsj.carranza@gmail.com>
2021-07-27 21:41:46 -0500

    imported x icon and added on click event to clear search input,added search function as well

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import { XIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import {useRouter} from 'next/router';
 import {useRef} from 'react';
@@ -16,6 +17,9 @@ function Header() {
             />
             <form className='flex px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center'>
                 <input ref={searchInputRef} className='flex-grow w-full focus:outline-none' type='text'/>
+                <XIcon className='h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125'
+                       onClick={() => (searchInputRef.current.value='')}
+                />
             </form>
         </header>
     )
