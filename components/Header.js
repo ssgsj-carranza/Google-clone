8d3559f2a84b91d450dce3893490b7f91a8471c9
unknown <ssgsj.carranza@gmail.com>
2021-07-27 21:29:01 -0500

    building and styling search bar in search page

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,22 @@
-import Image from "next/image"
+import Image from "next/image";
+import {useRouter} from 'next/router';
+import {useRef} from 'react';
 
 function Header() {
+    const router = useRouter();
+    const searchInputRef = useRef(null);
+
     return (
         <header>
             <Image src='https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png' 
                     height={40} 
                     width={120}
-                    className='cursor-pointer'/>
+                    onClick={() => router.push('/')}
+                    className='cursor-pointer'
+            />
+            <form className='flex px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center'>
+                <input ref={searchInputRef} className='flex-grow w-full focus:outline-none' type='text'/>
+            </form>
         </header>
     )
 }
