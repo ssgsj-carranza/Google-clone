d1ca7708db82eefeb54a8947e77a7a8c48fc1f89
unknown <ssgsj.carranza@gmail.com>
2021-07-28 21:12:50 -0500

    divided header options into 2 different divs, left and right, created new single component header option

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import HeaderOption from "./HeaderOption"
 
 function HeaderOptions() {
     return (
         <div>
-            
+            {/*left section */}
+            <div className="flex space-x-6">
+               <HeaderOption /> 
+            </div>
+            {/* right section*/}
+            <div className="flex space-x-4">
+                <p className='link'>Settings</p>
+                <p className='link'>Tools</p>
+            </div>
         </div>
     )
 }
